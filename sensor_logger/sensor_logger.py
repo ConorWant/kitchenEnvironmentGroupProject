32466ec786df05b1225c483e48329e893540ba41
Conor Want <hello@conor-want.com>
2026-03-04 08:04:43 +0000

    Completed sensor logger script. Added bme and light sensor functions, main loop and cleanup code.

--- sensor_logger/sensor_logger.py
+++ sensor_logger/sensor_logger.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import csv
 import time
 import os
@@ -5,49 +7,84 @@ from datetime import datetime
 import RPi.GPIO as GPIO
 from smbus2 import SMBus
 from bme280 import BME280
+import spidev
 
+#Settings
 LOG_INTERVAL = 30
 CSV_FILENAME = "sensor_log.csv"
 
+# Sensor initialization
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 i2c_bus = SMBus(1)
 bme280 = BME280(i2c_dev=i2c_bus)
 
-file_exists = os.path.exisits(CSV_FILENAME)
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 1000000
+spi.mode = 0b00
+
+# Sets up output file
+file_exists = os.path.exists(CSV_FILENAME)
 csv_file = open(CSV_FILENAME, "a", newline="")
 writer = csv.writer(csv_file)
 
 if not file_exists:
-
     writer.writerow([
         "timestamp",
         "temperature_c",
         "humidity_pct",
         "pressure_hpa",
-	"door_status",
-	#"light_lux", DELETE COMMENT WHEN LIGHT SENSOR IS WORKING
+        "door_status",
+        "light_level",
     ])
     print("Created new log file:", CSV_FILENAME)
 else:
     print("Appending to existing log file:", CSV_FILENAME)
 
+# Helper functions
+def read_door_status(pin):
+    if GPIO.input(pin) == 0:
+        return "CLOSED"
+    else:
+        return "OPEN"
 
-def read_door_status(GPIO_pin):
-	if GPIO.input(pin) ==  0:
-		return "CLOSED"
-	else:
-		return "OPEN"
 
-def read_bme_sensor:
-	pass
-	#BME sensor code goes here
+def read_bme_sensor():
+    temperature = round(bme280.get_temperature(), 2)
+    humidity    = round(bme280.get_humidity(), 2)
+    pressure    = round(bme280.get_pressure(), 2)
+    return temperature, humidity, pressure
 
-def read_light_sensor:
-	pass
-	#Light sensor code goes here
 
-# Main Loop goes here
+def read_light_sensor():
+    resp = spi.xfer2([0x00, 0x00])
+    raw = ((resp[0] & 0x1F) << 8 | resp[1]) >> 1
+    light = min(255, int(raw / 4096 * 255))
+    return light
 
-# Cleanup code goes here
+
+# Throws away first reading as it is unreliable
+bme280.get_temperature()
+time.sleep(2)
+# Main loop
+try:
+    while True:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        door = read_door_status(5)
+        temperature, humidity, pressure = read_bme_sensor()
+        light = read_light_sensor()
+        writer.writerow([timestamp, door, temperature, humidity, pressure, light])
+        csv_file.flush()
+        print(f"{timestamp} | Door Status: {door} | "
+              f"Temp: {temperature}C | Humidity: {humidity}% | Pressure: {pressure}hPa | Light: {light}")
+        time.sleep(LOG_INTERVAL)
+# Cleanup
+except KeyboardInterrupt:
+    print("\nStopped.")
+
+finally:
+    csv_file.close()
+    GPIO.cleanup()
+    spi.close()
